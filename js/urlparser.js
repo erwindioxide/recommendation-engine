0b71a2e869c668253cd1b94dc73ccea6ac403c30
Erwin Baluyot <41769372+erwindioxide@users.noreply.github.com>
2019-12-30 21:41:28 +0800

    Form Integration

--- js/urlparser.js
+++ js/urlparser.js
@@ -1,17 +1,17 @@
 function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+    let vars = {};
+    let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
         vars[key] = value;
     });
     return vars;
 }
 
-var code = (getUrlVars()["fundmanagercode"]).toLowerCase();
-var id = getUrlVars()["id"];
-var app = document.querySelector('.app');
-var api = 'https://v2-api.sheety.co/rampverfinancials/mfapi/' + code + '/' + id;
+let code = (getUrlVars()["fundmanagercode"]).toLowerCase();
+let id = getUrlVars()["id"];
+let app = document.querySelector('.app');
+let api = 'https://v2-api.sheety.co/rampverfinancials/mfapi/' + code + '/' + id;
 
-var renderData = "json." + code;
+let renderData = "json." + code;
 
 function dataFill(json) {
     document.getElementById("fundtitle").innerHTML = eval(renderData + ".fundname");
@@ -36,13 +36,17 @@ function dataFill(json) {
     document.getElementById("exitfee").innerHTML = eval(renderData + ".exitfee");
     document.getElementById("prospectuslink").href = eval(renderData + ".prospectuslink");
     document.getElementById("ffslink").href = eval(renderData + ".ffslink");
-    document.getElementById("formstring").href = eval(renderData + ".formstring");
-
-
+    document.getElementById("profilelink").href = code + ".html#profilelink";
+    document.getElementById("forms").href = code + ".html#forms";
+    document.getElementById("funding").href = code + ".html#funding";
+    document.getElementById("formstring").action = eval(renderData + ".formstring");
+    document.getElementById("fundnamevalue").value = eval(renderData + ".fundname");
+    document.getElementById("wpcode").value = eval(renderData + ".wpcode");
+    document.getElementById("wpcodetag").value = "wpcf7-f" + eval(renderData + ".wpcode") + "-o1eval";
 }
 
 
-var output = fetch(api)
+let output = fetch(api)
     .then(response => response.json())
     .then(json => {
         this.dataFill(json);
